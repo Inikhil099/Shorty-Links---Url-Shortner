fbed24dd06a3a8e82f8426dce9607363db7066da
Nikhil <nikhilraikwar099@gmail.com>
2025-07-18 23:35:10 +0530

    updated

---
+++ frontend/pages/LoadingPage.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const dotVariants = {
+  start: { y: 0 },
+  bounce: {
+    y: [-6, 6, -6],
+    transition: {
+      duration: 0.6,
+      repeat: Infinity,
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const LoadingScreen = ({ isError = false }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.95 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+      className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-gradient-to-br from-blue-100 to-white"
+    >
+      {/* Spinner */}
+      <div className="relative w-20 h-20 mb-8">
+        <div className="absolute inset-0 border-4 border-blue-500 border-dashed rounded-full animate-spin" />
+        {!isError && (
+          <>
+            <div className="absolute inset-4 bg-blue-500 rounded-full animate-ping opacity-20" />
+            <div className="absolute inset-6 bg-blue-500 rounded-full" />
+          </>
+        )}
+      </div>
+
+      {/* Dots only when loading */}
+      {!isError && (
+        <div className="flex space-x-2 mt-4">
+          {[0, 1, 2].map((i) => (
+            <motion.span
+              key={i}
+              className="w-3 h-3 bg-blue-500 rounded-full"
+              variants={dotVariants}
+              initial="start"
+              animate="bounce"
+              transition={{
+                delay: i * 0.2,
+                repeatDelay: 0.2,
+              }}
+            />
+          ))}
+        </div>
+      )}
+
+      {/* Message */}
+      <p
+        className={`mt-6 text-lg font-medium tracking-wide animate-pulse ${
+          isError ? 'text-red-600' : 'text-blue-600'
+        }`}
+      >
+        {isError
+          ? "Couldn't find Your Details. Redirecting to login..."
+          : 'Loading user data...'}
+      </p>
+    </motion.div>
+  );
+};
+
+export default LoadingScreen;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,59 +1,148 @@
+// import { useEffect, useState, useSyncExternalStore } from 'react'
+// import './App.css'
+// import {BrowserRouter,Routes,Route, Navigate, useNavigate} from "react-router-dom"
+// import Home from "../pages/Home"
+// import Signup from "../pages/Signup"
+// import Login from "../pages/Login"
+// import {useDispatch, useSelector} from "react-redux"
+// import axios from 'axios'
+// import { setUserInfo } from '../redux/slices/UserSlice'
+
+// const AuthRoute = ({children})=>{
+//   const userinfo = useSelector((state)=>state.userinfo.uservalue)
+//   const isAuthenticated = !!userinfo
+//   return isAuthenticated ?  <Navigate to={"/"}/> : children
+
+// }
+
+// const ProtectRoutes = ({children})=>{
+//   const userinfo = useSelector((state)=>state.userinfo.uservalue)
+//   const isAuthenticated = !!userinfo
+//   return isAuthenticated ? children : <Navigate to={"/login"}/>
+
+// }
+
+// function App() {
+//   const userinfo = useSelector((state)=>state.userinfo.uservalue)
+//   const dispatch = useDispatch()
+
+//   const getUserData = async()=>{
+//     try {
+//     const res = await axios.get("http://localhost:3000/user/get-user-data",{withCredentials:true})
+//     if(res.status == 200 && res.data.userdetails._id){
+//       dispatch(setUserInfo(res.data.userdetails))
+//     }
+//     } catch (error) {
+//       dispatch(setUserInfo(undefined))
+//     }
+//   }
+
+//   useEffect(() => {
+//     if(!userinfo){
+//       getUserData()
+//     }
+//   }, [userinfo])
+  
+
+//   return (
+//     <>
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path='/*' element={<Navigate to={"/signup"}/>}/>
+//       <Route path='/' element={<ProtectRoutes><Home/></ProtectRoutes>}/>
+//       <Route path='/login' element={<AuthRoute><Login/></AuthRoute>}/>
+//       <Route path='/signup' element={<AuthRoute><Signup/></AuthRoute>}/>
+//     </Routes>
+//     </BrowserRouter>
+
+
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
 import { useEffect, useState, useSyncExternalStore } from 'react'
 import './App.css'
-import {BrowserRouter,Routes,Route, Navigate, useNavigate} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom"
 import Home from "../pages/Home"
 import Signup from "../pages/Signup"
 import Login from "../pages/Login"
-import {useDispatch, useSelector} from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import axios from 'axios'
 import { setUserInfo } from '../redux/slices/UserSlice'
+import LoadingScreen from '../pages/LoadingPage'
 
-const AuthRoute = ({children})=>{
-  const userinfo = useSelector((state)=>state.userinfo.uservalue)
+const AuthRoute = ({ children }) => {
+  const userinfo = useSelector((state) => state.userinfo.uservalue)
   const isAuthenticated = !!userinfo
-  return isAuthenticated ?  <Navigate to={"/"}/> : children
+  return isAuthenticated ? <Navigate to={"/"} /> : children
 
 }
 
-const ProtectRoutes = ({children})=>{
-  const userinfo = useSelector((state)=>state.userinfo.uservalue)
+const ProtectRoutes = ({ children }) => {
+  const userinfo = useSelector((state) => state.userinfo.uservalue)
   const isAuthenticated = !!userinfo
-  return isAuthenticated ? children : <Navigate to={"/login"}/>
+  return isAuthenticated ? children : <Navigate to={"/login"} />
 
 }
 
 function App() {
-  const userinfo = useSelector((state)=>state.userinfo.uservalue)
+  const userinfo = useSelector((state) => state.userinfo.uservalue)
   const dispatch = useDispatch()
+  const [loading, setloading] = useState(true)
+  const [isError, setisError] = useState(false)
 
-  const getUserData = async()=>{
-    try {
-    const res = await axios.get("http://localhost:3000/user/get-user-data",{withCredentials:true})
-    if(res.status == 200 && res.data.userdetails._id){
-      dispatch(setUserInfo(res.data.userdetails))
-    }
-    } catch (error) {
-      dispatch(setUserInfo(undefined))
-    }
-  }
+
 
   useEffect(() => {
-    if(!userinfo){
+    const getUserData = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/user/get-user-data", { withCredentials: true })
+        if (res.status == 200 && res.data.userdetails._id) {
+          dispatch(setUserInfo(res.data.userdetails))
+          setTimeout(() => {
+            setloading(false)
+          }, 2000);
+        }
+      } catch (error) {
+        if (error) {
+          setTimeout(() => {
+            dispatch(setUserInfo(undefined))
+            setisError(true)
+            setTimeout(() => {
+              setloading(false)
+            }, 3000);
+          }, 5000);
+        }
+
+
+      }
+    }
+    if (!userinfo) {
       getUserData()
     }
+
   }, [userinfo])
-  
+
+  if (loading) {
+    return <LoadingScreen isError={isError} />
+  }
+
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/*' element={<Navigate to={"/signup"}/>}/>
-      <Route path='/' element={<ProtectRoutes><Home/></ProtectRoutes>}/>
-      <Route path='/login' element={<AuthRoute><Login/></AuthRoute>}/>
-      <Route path='/signup' element={<AuthRoute><Signup/></AuthRoute>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<Navigate to={"/signup"} />} />
+          <Route path='/' element={<ProtectRoutes><Home /></ProtectRoutes>} />
+          <Route path='/login' element={<AuthRoute><Login /></AuthRoute>} />
+          <Route path='/signup' element={<AuthRoute><Signup /></AuthRoute>} />
+        </Routes>
+      </BrowserRouter>
 
 
 
