5f1c057e0b1f653600bfe9a6317c99baba013aa1
Nikhil <nikhilraikwar099@gmail.com>
2026-04-15 01:12:06 +0530

    done

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = " https://expert-festival-communities-approved.trycloudflare.com";
+const baseURL = "https://promoting-trained-however-abstract.trycloudflare.com";
 export const backend_url = axios.create({ baseURL });
 backend_url.interceptors.request.use((cfg) => {
   const token = localStorage.getItem("token");
