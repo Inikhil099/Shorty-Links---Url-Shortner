f5778676a8e510c49e3113f396b02ac8676a0de2
Nikhil <nikhilraikwar099@gmail.com>
2026-05-02 17:38:23 +0530

    done

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = import.meta.env.VITE_API_URL;
+const url = ""
 const baseURL = url;
 export const backend_url = axios.create({ baseURL });
 backend_url.interceptors.request.use((cfg) => {
