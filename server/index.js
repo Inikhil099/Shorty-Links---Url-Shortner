94e69166b1868e2f3afc241a70e1d2c685cb46d7
Nikhil <nikhilraikwar099@gmail.com>
2026-05-03 20:12:14 +0530

    deployed

--- server/index.js
+++ server/index.js
@@ -64,8 +64,8 @@ app.get("/health", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(_dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(_dirname, "frontend", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
   });
 }
 
